c8cc38d2a75fe3e2337e819f48029635f3b298cc
ngqiaofang <93829712+ngqiaofang@users.noreply.github.com>
2023-12-29 05:30:59 +0800

    Add files via upload

---
+++ pages/3_💡Prediction.py
@@ -0,0 +1,213 @@
+import streamlit as st
+import pandas as pd
+from io import StringIO
+from streamlit_extras import add_vertical_space as avs
+from streamlit_extras.row import row
+from streamlit_extras.switch_page_button import switch_page 
+import joblib
+import re
+import string
+import nltk
+import emoji
+import advertools as adv
+import demoji
+from collections import Counter
+from nltk.stem import WordNetLemmatizer
+
+st.set_page_config(
+    page_title="Suicide Ideation Prediction",
+    page_icon="💡",
+    layout="wide",
+)
+
+st.title("Suicide Ideation Prediction")
+
+st.sidebar.markdown("## Do you need help?")
+st.sidebar.link_button("Helplines", "https://findahelpline.com/countries/my/topics/suicidal-thoughts")
+
+
+quote = f"""
+    <style>
+    .regular {{
+        position: relative;
+        padding-top:20px;
+        padding-bottom: 50px;
+        text-align: center;
+        text-size-adjust: auto;
+        align-self: center;
+
+        &::before {{
+            content: "“";
+            position: relative;
+            font-weight: bold;
+            font-size: 3em;
+            color: #f8a811;
+            top: -20px;
+            left: -15px;
+            font-family: Georgia, Times, serif;
+        }}
+        &::after {{
+            content: "”";
+            position: relative;
+            font-weight: bold;
+            font-size: 3em;
+            color: #f8a811;
+            bottom: -70px;
+            right: -15px;
+            font-family: Georgia, Times, serif;
+        }}
+    }}
+
+    </style>
+    <div>
+        <div>
+            <div class = 'regular'>There is some good in this world, and it’s worth fighting for.</div>
+        </div>
+        <p style="text-align:center;">- <span style="font-style: italic">J.R.R. Tolkien</span></p>
+    </div>
+
+"""
+
+
+######################################################################################################################
+# model #
+######################################################################################################################
+
+###### model & vectorizer import ##########
+model = joblib.load('./logistic_regression_model.joblib')
+vectorizer = joblib.load('./vectorizer.pkl')
+
+st.subheader('Let\'s try to predict your text:')
+
+####################### user input ######################
+
+st.markdown("Choose the input method you prefer:")
+rows = row(2, vertical_align="center")
+but1 = rows.button("Text", use_container_width=True)
+but2 = rows.button("File", use_container_width=True)
+# inputType = st.radio("",["Text", "File"], horizontal=True)
+
+if but2:
+    inputType = 'File'
+else:
+    inputType = 'Text'
+
+avs.add_vertical_space(1) 
+
+if inputType == 'File':
+    submitted = st.file_uploader("Upload a .txt file. This might take some times to load the result.", type=("txt"))
+    if submitted:
+        stringio=StringIO(submitted.getvalue().decode('utf-8'))
+        text=stringio.read()
+else:
+    with st.form("myform"):
+        text = st.text_input("Enter text and click submit. This might take some times to load the result.", "")
+        submitted = st.form_submit_button("Submit")
+
+######################## model
+avs.add_vertical_space(2) 
+if not submitted:
+    modelpage = st.button("Look into how's the prediction system works")
+    if modelpage:
+        switch_page("system")
+
+####### preprocess text ##########
+
+# nltk.download('punkt')
+# nltk.download('wordnet')
+# nltk.download('omw-1.4')
+# nltk.download('stopwords')
+
+wordnet_lemmatizer = WordNetLemmatizer()
+
+def preprocessing_text(text):
+    text = re.sub(r'http\S+', '', text) 
+    text = "".join([i for i in text if i not in string.punctuation])
+    text = text.replace("’", "")
+    text = " ".join(text.split())
+    text = text.lower()
+
+    tokens = nltk.word_tokenize(text)
+    stopwords = nltk.corpus.stopwords.words('english')
+    tokens = [i for i in tokens if i not in stopwords]
+    tokens = [wordnet_lemmatizer.lemmatize(i) for i in tokens]
+    text = ' '.join(map(str,tokens))
+
+    return text
+
+
+####### feature extraction ################
+
+def mostfrequencyemoji(emojies):
+    if emojies == []:
+        final = ''
+    else:
+        final = Counter(emojies)
+        final = max(final, key=final.get)
+
+    return final
+
+
+
+emojidf = adv.emoji_df
+
+def emoji_extraction(text):
+    emojis = [x for x in text if emoji.is_emoji(x)]
+    emojies_count = emoji.emoji_count(text)
+    unique_emojies_count = emoji.emoji_count(text, unique=True)
+    mostfreqemoji = mostfrequencyemoji(emojis)
+    emojilabel = emojidf.index[emojidf['emoji'] == mostfreqemoji].tolist()
+    if len(emojilabel) < 1:
+        emojilabel = 0
+    else:
+        emojilabel = emojilabel[0] + 1
+
+    df = {'emojies_count': emojies_count,
+    'unique_emojies_count': unique_emojies_count,
+    'most_freq_emoji': emojilabel}
+
+    return df
+
+
+def text_vectorization(text, df1):
+    text = [demoji.replace_with_desc(text, " ")]
+    unigram = vectorizer.transform(text)
+    unigram = pd.DataFrame.sparse.from_spmatrix(unigram)
+    unigram = unigram.iloc[-1:]
+    unigram['emojies_count'] = df1['emojies_count']
+    unigram['unique_emojies_count'] = df1['unique_emojies_count']
+    unigram['most_freq_emoji'] = df1['most_freq_emoji']
+    
+    return unigram
+
+def features_extraction(text):
+    df1 = emoji_extraction(text)
+    unigram = text_vectorization(text, df1)
+    return unigram
+
+######################################################################################################################
+# implement model + result
+######################################################################################################################
+
+if submitted:
+    text = preprocessing_text(text)
+    features = features_extraction(text)
+    X = features.iloc[:, :].values
+    y_pred = model.predict(X)
+    if y_pred == 0:
+        st.balloons()
+        st.subheader("Congratulations! Your text is classified as non-suicide text!")
+        modelpage = st.button("Look into how's the prediction system works")
+        if modelpage:
+            switch_page("system")
+    else:
+        st.subheader("Your text is classified as suicide text.")
+        st.markdown(quote, unsafe_allow_html=True)
+        col1, col2, col3 = st.columns([1,3,1])
+        with col2:
+            st.link_button("Find a helpline here", "https://findahelpline.com/countries/my/topics/suicidal-thoughts", use_container_width=True)
+            avs.add_vertical_space(1)
+
+            modelpage = st.button("Look into how's the prediction system works", use_container_width=True)
+            if modelpage:
+                switch_page("system")    
---
+++ pages/4_💻System.py
@@ -0,0 +1,137 @@
+import streamlit as st
+import plotly.express as px
+from streamlit_extras import add_vertical_space as avs 
+import pandas as pd
+from streamlit_extras.switch_page_button import switch_page 
+
+
+st.set_page_config(
+    page_title="About the System",
+    page_icon="💻",
+    layout="wide",
+)
+
+st.title("About the System")
+
+st.sidebar.markdown("## Do you need help?")
+st.sidebar.link_button("Helplines", "https://findahelpline.com/countries/my/topics/suicidal-thoughts")
+
+######################
+# class distribution #
+######################
+
+data = {'Non-suicide':5054, 'Suicide':4946}
+classes = list(data.keys())
+number = list(data.values())
+
+fig = px.bar(x=classes, y =number, title = "Class Ditribution of Sampling Dataset",
+            labels={'x': " ", 'y': " "})
+fig.update_xaxes(showgrid=False)
+fig.update_yaxes(showgrid=False)
+fig.update_traces(marker_color='#f8a811')
+
+avs.add_vertical_space(1)
+
+selected = st.selectbox("Choose one part of the system to focus on it:", ["Dataset", "Model", "Features"])
+
+
+
+avs.add_vertical_space(1)
+
+if selected == "Dataset":
+    dataurl = "https://www.kaggle.com/datasets/nikhileswarkomati/suicide-watch"
+
+    dataset= f"""    
+    <p>The <a href={dataurl}>dataset </a>chosen comprises of 232,074 texts and is 
+    categorized into two classes: suicide and non-suicide. It has been undersampled to 10,000 texts for training data.</p>
+    """
+
+    
+    st.subheader("Distribution of Classes")
+
+    st.markdown(dataset, unsafe_allow_html=True)
+
+    st.plotly_chart(fig, use_container_width=True)
+    st.write("The undesampled data is balanced, as both classes have nearly equal representation.")
+
+
+    avs.add_vertical_space(4)
+
+    # st.divider()
+    st.subheader('Sample Texts')   
+
+    data = {
+        "Texts": ["It ends tonight.I can't do it anymore. I quit.", "Day 1: Useless facts until quarintine is over Sharks are the only fish that can blink with both eyes."],
+        "Classes": ["suicide","non-suicide"]
+    }
+    df = pd.DataFrame(data)
+    st.dataframe(data,hide_index=True, use_container_width=True)
+
+
+
+if selected == "Model":
+    scikiturl = "https://scikit-learn.org/0.16/modules/generated/sklearn.linear_model.LogisticRegression.html"
+    scikit= f"""    
+    <p>The calssification model used is <span style="font: bold; color: #f8a811">Logistic Regression</span> from
+    <a href={scikiturl}>scikit-learn </a>python library.</p>
+    """
+    st.subheader('Logistic Regression')
+    st.markdown(scikit, unsafe_allow_html=True)
+    st.markdown("The paremeters are set to default except for:")
+    st.markdown("- Solver: liblinear")
+    st.markdown("- Max_iter: 10000")
+
+    st.text("'liblinear' is a solver that recommended when we have high dimension dataset (recommended for solving large-scale classification problems)")
+# “liblinear” (A Library for Large Linear Classification):
+
+# It’s a linear classification that supports logistic regression and linear support vector machines.
+
+# The solver uses a Coordinate Descent (CD) algorithm that solves optimization problems by successively performing approximate minimization along coordinate directions or coordinate hyperplanes.
+
+# liblinear applies L1 Regularization and it’s recommended when we have high dimension dataset (recommended for solving large-scale classification problems).
+
+    avs.add_vertical_space(2)
+    st.subheader('Result of Training Model')
+    # plot_confusion_matrix(model, x_test, y_test, display_labels=   class_names)
+    #     st.pyplot()
+    data = {
+        "Metics": ["Accuracy", "F1-score", "Precision", "Recall"],
+        "Values": ["0.91","0.90", "0,91", "0.91"]
+    }
+    df = pd.DataFrame(data)
+    st.dataframe(data,hide_index=True, use_container_width=True)
+
+
+
+    # # Imports
+    # from mlxtend.plotting import plot_confusion_matrix
+    # import matplotlib.pyplot as plt
+    # import numpy as np
+
+    # # Your Confusion Matrix
+    # cm = np.array([[1401, 85],
+    #                 [ 200, 1314]])
+
+    # # Classes
+    # classes = ['non-suicide', 'suicide']
+
+    # figure, ax = plot_confusion_matrix(conf_mat = cm,
+    #                                 class_names = classes,
+    #                                 show_absolute = False,
+    #                                 show_normed = True,
+    #                                 colorbar = True)
+    # st.pyplot(figure)
+
+
+if selected == "Features":
+    st.subheader('Features Selected:')
+    st.markdown("- Text vectorization, including emoji-converted text, using unigram")
+    st.markdown("- Count of emoji in the text")
+    st.markdown("- Most frequent used emoji in the text")
+    
+avs.add_vertical_space(2)
+col1, col2, col3 = st.columns([1,3,1])
+with col2:
+    predictpage = st.button("Let's try to predict your text", use_container_width=True)
+    if predictpage:
+        switch_page("prediction")
