4be3b97523e2a57c81681292c70f5747ea7cf0c6
ngqiaofang <93829712+ngqiaofang@users.noreply.github.com>
2023-12-29 05:33:11 +0800

    Update 3_💡Prediction.py

--- pages/3_💡Prediction.py
+++ pages/3_💡Prediction.py
@@ -113,10 +113,10 @@ if not submitted:
 
 ####### preprocess text ##########
 
-# nltk.download('punkt')
-# nltk.download('wordnet')
-# nltk.download('omw-1.4')
-# nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('wordnet')
+nltk.download('omw-1.4')
+nltk.download('stopwords')
 
 wordnet_lemmatizer = WordNetLemmatizer()
 
